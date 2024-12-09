b7bf01ee78f42bbb27487b85b9ff3b90ce8e21c7
Supradeep Mukherjee <supradeep2004@gmail.com>
2024-12-09 21:56:49 +0530

    Implemented Security Measures

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import { config } from "dotenv";
 import morgan from "morgan";
 import express from "express";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import expressMongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import cookieParser from "cookie-parser";
+import cors from 'cors'
 
 config()
 
@@ -8,6 +14,18 @@ const port = process.env.PORT
 const nodeEnv = process.env.NODE_ENV
 
 const app = express()
+
+const limit = rateLimit({
+    windowMs: 15 * 60000,
+    limit: 100,
+    message: 'Too many requests, please try again later'
+})
+
+app.use(helmet())
+app.use(expressMongoSanitize())
+app.use(hpp())
+app.use(cookieParser())
+app.use('/api', limit)
 
 if (nodeEnv === 'development') app.use(morgan('dev'))
 app.use(express.json({ limit: '10kb' }))
@@ -24,6 +42,13 @@ app.use((err, req, res, next) => {
         ...(nodeEnv === 'development' && { stack: err.stack })
     })
 })
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+}))
 
 app.use((req, res) => res.status(404).json({
     status: 'error',
